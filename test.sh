0f0233af2bde2d3ac896e164f9812464a4378ead
avi <gangoll1992@gmail.com>
2020-12-20 13:38:44 +0200

    test

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-check=`curl -i  http://18.195.32.244:200 | grep -c "HTTP/1.1 300 "`
+check=`curl -i  http://18.195.32.244:200 | grep -c "HTTP/1.1 200"`
 if [ $check -eq 1 ] 
 then
 echo "test success"
